0a604aec77d1bc0fc3ef2c05c11e4a36e7223bc4
Alan Davidson <alan@viam.com>
2022-10-19 16:22:18 -0400

    enable DirectionFlip in the tests again

--- components/motor/gpio/motor_encoder_test.go
+++ components/motor/gpio/motor_encoder_test.go
@@ -38,7 +38,7 @@ func TestMotorEncoder1(t *testing.T) {
 	undo := SetRPMSleepDebug(1, false)
 	defer undo()
 
-	cfg := Config{TicksPerRotation: 100, MaxRPM: 100}//, DirectionFlip: true
+	cfg := Config{TicksPerRotation: 100, MaxRPM: 100, DirectionFlip: true}
 	fakeMotor := &fakemotor.Motor{
 		MaxRPM:           100,
 		Logger:           logger,
@@ -279,7 +279,7 @@ func TestMotorEncoderIncremental(t *testing.T) {
 	}
 	setup := func(t *testing.T) testHarness {
 		t.Helper()
-		cfg := Config{TicksPerRotation: 100, MaxRPM: 100}//, DirectionFlip: true
+		cfg := Config{TicksPerRotation: 100, MaxRPM: 100, DirectionFlip: true}
 		fakeMotor := &fakemotor.Motor{
 			MaxRPM:           100,
 			Logger:           logger,
